1ba2ff8fb93115909dd384bd9320272a0602971a
Project-OAO <ashgrayblue0@gmail.com>
2023-12-02 01:16:06 +0900

    programmers lv0 :: 안전지대 실패 코드 스타일만 변경

--- programmers/lv0/안전지대.js
+++ programmers/lv0/안전지대.js
@@ -4,131 +4,136 @@
 
 // 제대로 풀 지는 못했으나, 우선 기록용으로 작성
 function solution(board) {
-    let answer = [...board];
-    board.forEach((rowElement, row) => {
-        rowElement.forEach((columnElement, column) => {
-            let tmp = checkPlace(board, row, column);
-            switch(tmp) {
-                case 'vertex' :
-                    answer = figureVertex(board, row, column);
-                    break;
-                case 'side' :
-                    answer = figureSide(board, row, column);
-                    break;
-                case 'inner' :
-                    answer = figureInner(board, row, column);
-                    break;
-                case 'none' :
-                    break;
-                default :
-                    break;
-            }
-        });
+  let answer = [...board];
+  board.forEach((rowElement, row) => {
+    rowElement.forEach((columnElement, column) => {
+      const tmp = checkPlace(board, row, column);
+      switch (tmp) {
+        case 'vertex':
+          answer = figureVertex(board, row, column);
+          break;
+        case 'side':
+          answer = figureSide(board, row, column);
+          break;
+        case 'inner':
+          answer = figureInner(board, row, column);
+          break;
+        case 'none':
+          break;
+        default:
+          break;
+      }
     });
+  });
 
-    console.log(answer);
+  console.log(answer);
 
-    return answer.reduce((acc, cur) => {
-        let tmp = acc;
-        cur.forEach((e) => {
-            if (e === 0) tmp += 1;
-        });
-        return tmp;
-    }, 0);
+  return answer.reduce((acc, cur) => {
+    let tmp = acc;
+    cur.forEach(e => {
+      if (e === 0) tmp += 1;
+    });
+    return tmp;
+  }, 0);
 }
 
 function checkPlace(board, row, column) {
-    const finalIndex = board.length - 1;
+  const finalIndex = board.length - 1;
 
-    if (row === 0 && column === 0 && board[0][0] === 1) return 'vertex';
-    else if (row === 0 && column === finalIndex && board[0][finalIndex] === 1) return 'vertex';
-    else if (row === finalIndex && column === 0 && board[finalIndex][0] === 1) return 'vertex';
-    else if (row === finalIndex && column === finalIndex && board[finalIndex][finalIndex] === 1) return 'vertex';
-
-    else if (row === 0 && board[row][column] === 1) return 'side';
-    else if (row === finalIndex && board[row][column] === 1) return 'side';
-    else if (column === 0 && board[row][column] === 1) return 'side';
-    else if (column === finalIndex && board[row][column] === 1) return 'side';
-
-    else if (board[row][column] === 1) return 'inner';
-    return 'none';
+  if (row === 0 && column === 0 && board[0][0] === 1) return 'vertex';
+  if (row === 0 && column === finalIndex && board[0][finalIndex] === 1)
+    return 'vertex';
+  if (row === finalIndex && column === 0 && board[finalIndex][0] === 1)
+    return 'vertex';
+  if (
+    row === finalIndex &&
+    column === finalIndex &&
+    board[finalIndex][finalIndex] === 1
+  )
+    return 'vertex';
+  if (row === 0 && board[row][column] === 1) return 'side';
+  if (row === finalIndex && board[row][column] === 1) return 'side';
+  if (column === 0 && board[row][column] === 1) return 'side';
+  if (column === finalIndex && board[row][column] === 1) return 'side';
+  if (board[row][column] === 1) return 'inner';
+  return 'none';
 }
 
 function figureInner(board, row, column) {
-    const target = [...board];
-    console.log(`Inner : ${row}, ${column}`);
-    console.log(target);
+  const target = [...board];
+  console.log(`Inner : ${row}, ${column}`);
+  console.log(target);
 
-    target[row - 1][column - 1] = 1;
-    target[row - 1][column] = 1;
-    target[row - 1][column + 1] = 1;
+  target[row - 1][column - 1] = 1;
+  target[row - 1][column] = 1;
+  target[row - 1][column + 1] = 1;
+  target[row][column - 1] = 1;
+  target[row][column + 1] = 1;
+  target[row + 1][column - 1] = 1;
+  target[row + 1][column] = 1;
+  target[row + 1][column + 1] = 1;
+
+  console.log(target);
+
+  return target;
+}
+
+function figureSide(board, row, column) {
+  const target = [...board];
+  console.log(`side : ${row}, ${column}`);
+  const finIndex = board.length - 1;
+
+  if (row === 0) {
     target[row][column - 1] = 1;
     target[row][column + 1] = 1;
     target[row + 1][column - 1] = 1;
     target[row + 1][column] = 1;
     target[row + 1][column + 1] = 1;
+  } else if (row === finIndex) {
+    target[row - 1][column - 1] = 1;
+    target[row - 1][column] = 1;
+    target[row - 1][column + 1] = 1;
+    target[row][column - 1] = 1;
+    target[row][column + 1] = 1;
+  } else if (column === 0) {
+    target[row + 1][column] = 1;
+    target[row + 1][column + 1] = 1;
+    target[row][column + 1] = 1;
+    target[row - 1][column] = 1;
+    target[row - 1][column + 1] = 1;
+  } else if (column === finIndex) {
+    target[row - 1][column - 1] = 1;
+    target[row - 1][column] = 1;
+    target[row][column - 1] = 1;
+    target[row + 1][column - 1] = 1;
+    target[row + 1][column] = 1;
+  }
 
-    console.log(target);
-
-    return target;
-}
-
-function figureSide(board, row, column) {
-    const target = [...board];
-    console.log(`side : ${row}, ${column}`);
-    const finIndex = board.length - 1;
-
-    if (row === 0) {
-        target[row][column - 1] = 1;
-        target[row][column + 1] = 1;
-        target[row + 1][column - 1] = 1;
-        target[row + 1][column] = 1;
-        target[row + 1][column + 1] = 1;
-    } else if (row === finIndex) {
-        target[row - 1][column - 1] = 1;
-        target[row - 1][column] = 1;
-        target[row - 1][column + 1] = 1;
-        target[row][column - 1] = 1;
-        target[row][column + 1] = 1;
-    } else if (column === 0) {
-        target[row + 1][column] = 1;
-        target[row + 1][column + 1] = 1;
-        target[row][column + 1] = 1;
-        target[row - 1][column] = 1;
-        target[row - 1][column + 1] = 1;
-    } else if (column === finIndex) {
-        target[row - 1][column - 1] = 1;
-        target[row - 1][column] = 1;
-        target[row][column - 1] = 1;
-        target[row + 1][column - 1] = 1;
-        target[row + 1][column] = 1;
-    }
-    return target;
+  return target;
 }
 
 function figureVertex(board, row, column) {
-    const target = [...board];
-    console.log(`Vertex : ${row}, ${column}`);
-    if (row === 0) {
-        if (column === 0) {
-            target[row][column + 1] = 1;
-            target[row + 1][column] = 1;
-            target[row + 1][column + 1] = 1;
-        } else {
-            target[row][column - 1] = 1;
-            target[row + 1][column - 1] = 1;
-            target[row + 1][column] = 1;
-        }
+  const target = [...board];
+  console.log(`Vertex : ${row}, ${column}`);
+  if (row === 0) {
+    if (column === 0) {
+      target[row][column + 1] = 1;
+      target[row + 1][column] = 1;
+      target[row + 1][column + 1] = 1;
     } else {
-        if (column === 0) {
-            target[row][column + 1] = 1;
-            target[row - 1][column] = 1;
-            target[row - 1][column + 1] = 1;
-        } else {
-            target[row][column - 1] = 1;
-            target[row - 1][column] = 1;
-            target[row - 1][column - 1] = 1;
-        }
+      target[row][column - 1] = 1;
+      target[row + 1][column - 1] = 1;
+      target[row + 1][column] = 1;
     }
-    return target;
+  } else if (column === 0) {
+    target[row][column + 1] = 1;
+    target[row - 1][column] = 1;
+    target[row - 1][column + 1] = 1;
+  } else {
+    target[row][column - 1] = 1;
+    target[row - 1][column] = 1;
+    target[row - 1][column - 1] = 1;
+  }
+
+  return target;
 }
