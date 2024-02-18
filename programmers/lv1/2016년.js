b452f1091c23f8c0b1c16f0dd9ab7246cf167635
Project-OAO <ashgrayblue0@gmail.com>
2024-02-18 23:59:59 +0900

    programmers lv1 :: 2016년

--- programmers/lv1/2016년.js
+++ programmers/lv1/2016년.js
@@ -1,3 +1,4 @@
+// 그냥 풀었는데, 둠스데이 알고리즘이라는게 있다고 함. 그리고 이게 그 케이스.
 function solution(a, b) {
   const week = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
   const day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
