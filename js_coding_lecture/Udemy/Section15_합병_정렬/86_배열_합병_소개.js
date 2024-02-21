8cb319c160e5ba2f879273825d37e5064d1561ed
Project-OAO <ashgrayblue0@gmail.com>
2024-02-22 00:05:23 +0900

    Udemy Lecture - Section 14: Merge Sort - 합병 알고리즘 조금 더 최적화

--- js_coding_lecture/Udemy/Section15_합병_정렬/86_배열_합병_소개.js
+++ js_coding_lecture/Udemy/Section15_합병_정렬/86_배열_합병_소개.js
@@ -11,25 +11,33 @@ function merge(arr1, arr2) {
   return mergedArr;
 }
 
-// function merge2(arr1, arr2) {
-//   const mergedArr = [];
-//   let i = 0; // arr1 Index
-//   let j = 0; // arr2 Index
-//   while (true) {
-//     if (i === arr1.length) {
-//     }
-//
-//     if (arr1[i] >= arr2[j]) {
-//       mergedArr.push(arr2[j]);
-//       j += 1;
-//     } else {
-//       mergedArr.push(arr1[i]);
-//       i += 1;
-//     }
-//   }
-//
-//   return mergedArr;
-// }
+function merge2(arr1, arr2) {
+  const mergedArr = [];
+  let i = 0; // arr1 Index
+  let j = 0; // arr2 Index
+  for (; i < arr1.length && j < arr2.length; ) {
+    if (arr1[i] >= arr2[j]) {
+      mergedArr.push(arr2[j]);
+      j += 1;
+    } else {
+      mergedArr.push(arr1[i]);
+      i += 1;
+    }
+  }
+
+  if (i === arr1.length) {
+    for (; j < arr2.length; j++) {
+      mergedArr.push(arr2[j]);
+    }
+  } else if (j === arr2.length) {
+    for (; i < arr1.length; i++) {
+      mergedArr.push(arr1[i]);
+    }
+  }
+
+  return mergedArr;
+}
 
 console.log(merge([2], [1]));
 console.log(merge2([1, 2], [3, 4]));
+console.log(merge2([1, 2, 7, 9], [3, 4, 5, 6]));
