3a36b9fbbcc9b8eef0963c91cc616ec4edb1a89e
zen <ashgrayblue0@gmail.com>
2024-03-25 05:01:25 +0900

    Udemy Lecture - Section 29 : 동적프로그래밍 - 내가 구현한 코드 + 강사님 코드

--- js_coding_lecture/Udemy/Section29_동적프로그래밍/Fibonacci_dp.js
+++ js_coding_lecture/Udemy/Section29_동적프로그래밍/Fibonacci_dp.js
@@ -4,9 +4,17 @@ function fibonacci_dp(n) {
   function helper(n) {
     if (n <= 1 && !memo[n]) memo[n] = 1;
     if (!memo[n]) memo[n] = helper(n - 1) + helper(n - 2);
-    
+
     return memo[n];
   }
 
   return helper(n);
+}
+
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  const res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
 }
