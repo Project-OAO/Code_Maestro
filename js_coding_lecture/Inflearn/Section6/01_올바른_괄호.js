38c4099de911900e4a7ca401071c9e1d2c807523
effozen <ashgrayblue0@gmail.com>
2024-07-15 09:38:15 +0900

    feat: 인프런 강의 섹션6 :: 스택, 큐

---
+++ js_coding_lecture/Inflearn/Section6/01_올바른_괄호.js
@@ -0,0 +1,17 @@
+const answer = (input) => {
+  const stack = [];
+  const tmp = input.split('');
+  let result = 'YES';
+
+  tmp.forEach(v => {
+    if (v === '(') stack.push(v);
+    else if (stack.length !== 0) result = 'NO';
+    else stack.pop();
+  });
+
+  if (stack.length !== 0) result = 'NO';
+
+  return result;
+};
+
+console.log(answer('(()(()))(()'));
