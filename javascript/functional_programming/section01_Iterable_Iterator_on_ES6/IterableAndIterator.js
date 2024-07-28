563396f53b8a14de3b058f799fff270a4759857b
effozen <ashgrayblue0@gmail.com>
2024-07-29 00:14:35 +0900

    feat: custom Iterable/Iterator 객체 구현

---
+++ javascript/functional_programming/section01_Iterable_Iterator_on_ES6/IterableAndIterator.js
@@ -0,0 +1,14 @@
+const Iterable = {
+  [Symbol.iterator]() {
+    let i = 3;
+    return {
+      next() {
+        return i === 0 ? {done: true} : {value: i--, done: false};
+      },
+      [Symbol.iterator]() {
+        return this;
+      }
+    };
+  }
+};
+
